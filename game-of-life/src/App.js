3235d4c5b59bef56c170bc1660ce0a6411d5ccfc
annebrandes <98980909+annebrandes@users.noreply.github.com>
2023-07-24 13:56:00 -0400

    styling

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <h1> Conway's Game of Life </h1>
+      <p className = "Credits"> <em> Credits to Mathematician <a href = "https://en.wikipedia.org/wiki/John_Horton_Conway"> John Horton Conway </a> </em> </p>
     </div>
   );
 }

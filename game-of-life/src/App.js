b3ddeaeb095923a99994081446fce69216781341
annebrandes <98980909+annebrandes@users.noreply.github.com>
2023-07-24 18:12:16 -0400

    README

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1> Conway's Game of Life </h1>
         <p className = "Credits"><em> Credits to Mathematician <a href = "https://en.wikipedia.org/wiki/John_Horton_Conway"> John Horton Conway </a> </em> </p>
       </div>
-      <p className = "Instructions">  Click directly on squares to mark them as active. After you've created your optimal initial configuration, click start. </p>
+      <p className = "Instructions">  Click directly on squares to mark them as active. After you've created your optimal initial configuration, click start. To halt the game and re-configure, click stop. </p>
       <Grid size={20} />
     </>
   );

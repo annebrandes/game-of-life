d3d7b6ea00d243c8552bdff81619cab2be0fabd9
annebrandes <98980909+annebrandes@users.noreply.github.com>
2023-07-24 14:57:24 -0400

    button functionality

--- game-of-life/src/Grid.js
+++ game-of-life/src/Grid.js
@@ -2,34 +2,39 @@ import React, { useState } from 'react';
 import './Grid.css';
 
 const Grid = ({ size = 20 }) => {
-    // Initialize a size x size grid with all cells set to "inactive"
     const initialGridState = Array(size).fill().map(() => Array(size).fill('inactive'));
     const [gridState, setGridState] = useState(initialGridState);
 
-    // Function to handle cell click
     const handleCellClick = (i, j) => {
-        // Copy the current grid state
         const newGridState = [...gridState];
-        // Toggle the state of the cell at (i, j)
         newGridState[i][j] = newGridState[i][j] === 'active' ? 'inactive' : 'active';
-        // Update the grid state
         setGridState(newGridState);
     };
 
+    const handleGame = () => {
+        const newGridState = [...gridState];
+        setGridState(newGridState);
+    }
+
     return (
-        <div className="grid">
-            {gridState.map((row, i) => (
-                <div key={i} className="grid-row">
-                    {row.map((cell, j) => (
-                        <div
-                            key={`${i}-${j}`}
-                            className={`grid-cell ${cell}`} // This will apply either "grid-cell active" or "grid-cell inactive"
-                            onClick={() => handleCellClick(i, j)}
-                        />
-                    ))}
-                </div>
-            ))}
-        </div>
+        <>
+            <div className="grid">
+                {gridState.map((row, i) => (
+                    <div key={i} className="grid-row">
+                        {row.map((cell, j) => (
+                            <div
+                                key={`${i}-${j}`}
+                                className={`grid-cell ${cell}`} 
+                                onClick={() => handleCellClick(i, j)}
+                            />
+                        ))}
+                    </div>
+                ))}
+                <br />
+                <button className = "start" onClick = {() => handleGame()}> Start </button>
+                <br />
+            </div>
+        </>
     );
 };
 

3113929405072c7a6392c04a99c2d6059689441f
annebrandes <98980909+annebrandes@users.noreply.github.com>
2023-07-24 14:14:13 -0400

    grid with active and inactive squares

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Grid from './Grid';
 
 function App() {
-  
+
   return (
-    <div className="App">
-      <h1> Conway's Game of Life </h1>
-      <p className = "Credits"> <em> Credits to Mathematician <a href = "https://en.wikipedia.org/wiki/John_Horton_Conway"> John Horton Conway </a> </em> </p>
-    </div>
+    <>
+      <div className="App">
+        <h1> Conway's Game of Life </h1>
+        <p className = "Credits"> <em> Credits to Mathematician <a href = "https://en.wikipedia.org/wiki/John_Horton_Conway"> John Horton Conway </a> </em> </p>
+      </div>
+      <Grid size={20} />
+    </>
   );
 }
 
